35885e417e5960c7e347df1f049ed7cc98c68cdc
Simon <dev.simon.bengtsson@gmail.com>
2024-01-31 12:34:06 +0100

    decreased interval for some testing

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const HOUR_OF_BIRTHDAY_NOTIFICATION = 11; // The hour of the day on which the notification will be sent
 export const HOUR_OF_BEER_NOTIFICATION = 14; // The hour of the day on which the notification will be sent
 export const DAY_OF_BEER = 5; // Friday
-export const TIMER_DURATION = 1000 * 60 * 15; // Duration of the setTimeout, should be set to 15 minutes
+export const TIMER_DURATION = 1000 * 5; // Duration of the setTimeout, should be set to 15 minutes
 export const FILE_PATH = "../../users.json"; // Path to our json file

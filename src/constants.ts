91c61c384d599159f7b1473ee979e4937b029033
Simon <dev.simon.bengtsson@gmail.com>
2024-01-31 11:12:08 +0100

    set hour to 11

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const HOUR_OF_BIRTHDAY_NOTIFICATION = 9; // The hour of the day on which the notification will be sent
+export const HOUR_OF_BIRTHDAY_NOTIFICATION = 11; // The hour of the day on which the notification will be sent
 export const HOUR_OF_BEER_NOTIFICATION = 14; // The hour of the day on which the notification will be sent
 export const DAY_OF_BEER = 5; // Friday
 export const TIMER_DURATION = 1000 * 5; // should be set to 15 minutes
